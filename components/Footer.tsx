593d4ffd54a5bab6d93528b83cc858b9857230a3
askender <askender43@gmail.com>
2022-03-07 16:54:09 +0800

    show version

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,10 +12,10 @@ export const Footer  = memo(() => {
         <a href="https://discord.gg/QaEwmJMDJ2">Discord</a> &nbsp;
         <a href="https://github.com/dweb-lab/CCNP">Github</a> &nbsp;
         <a href="https://twitter.com/askender43">Twitter</a> &nbsp;
+        Ver: {FrontendVersion} &nbsp;
         Made with love by <a href="Dweb Lab">Dweb Lab</a>
       </p>
       <p className="hidden">
-        Frontend Version: {FrontendVersion} &nbsp;
         Backend Version: {backendVersion} &nbsp;
         & <a href="https://ipfs.io/">IPFS</a> &nbsp;
         & <a href="https://mumbai.polygonscan.com/">Polygon (MATIC) Mumbai TESTNET</a>
